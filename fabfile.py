a58613cab73330a816757c31710bbc41a5f29624
Joseph Botros <jrbotros@gmail.com>
2015-06-27 16:18:05 -0400

    Fix deploy script permissions, allow single file deployment

--- fabfile.py
+++ fabfile.py
@@ -1,6 +1,11 @@
+import os
 from fabric.api import local
 
 s3_path = "s3://www.josephbotros.com/"
+default_flags = "--acl public-read"
 
-def deploy():
-    local("aws s3 sync . {0}".format(s3_path))
+def deploy(name=None, flags=default_flags):
+    if name is None:
+        local("aws s3 sync . {0} {1}".format(s3_path, flags))
+    else:
+        local("aws s3 cp {0} {1} {2}".format(name, os.path.join(s3_path, name), flags))
